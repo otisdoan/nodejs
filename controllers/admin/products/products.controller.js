0f885d7a369e8256caf4f8858392f64340982f44
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-04-23 16:09:33 +0700

    update

--- controllers/admin/products/products.controller.js
+++ controllers/admin/products/products.controller.js
@@ -1,6 +1,15 @@
 const Products = require('../../../models/product.model')
 module.exports.index = async (req, res) => {
-  const products = await Products.find({});
+
+  let find = {
+    deleted: false
+  }
+
+  if (req.query.status) {
+    find.status = req.query.status
+  }
+
+  const products = await Products.find(find);
   const newProduct = products.map((item) => {
     item.displayStatus = item.status === 'active' ? 'Hoạt động' : 'Dừng hoạt động'
     item.classStatus = item.status === 'active' ? 'btn-success' : 'btn-danger'
