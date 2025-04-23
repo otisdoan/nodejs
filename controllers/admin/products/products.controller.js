3457d55d407714401241414499f2213d43e7aa7c
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-04-23 15:02:47 +0700

    update

--- controllers/admin/products/products.controller.js
+++ controllers/admin/products/products.controller.js
@@ -1,3 +1,13 @@
-module.exports.index = (req, res) => {
-  res.render('admin/pages/products/index');
+const Products = require('../../../models/product.model')
+module.exports.index = async (req, res) => {
+  const products = await Products.find({});
+  const newProduct = products.map((item) => {
+    item.displayStatus = item.status === 'active' ? 'Hoạt động' : 'Dừng hoạt động'
+    item.classStatus = item.status === 'active' ? 'btn-success' : 'btn-danger'
+    return item;
+  })
+  console.log(newProduct);
+  res.render('admin/pages/products/index', {
+    products: newProduct
+  });
 }
