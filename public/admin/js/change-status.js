b58dbbb60958a377ab3253abdb215d3fd108d7ac
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-04-28 23:48:44 +0700

    update

--- public/admin/js/change-status.js
+++ public/admin/js/change-status.js
@@ -1,5 +1,8 @@
 const buttonStatus = document.querySelectorAll('.status');
 const formElement = document.querySelector('.change-status');
+const checkBoxAll = document.querySelector("[name = 'checkall']");
+const checkBoxRecord = document.querySelectorAll("[name='id']");
+let total = 0;
 
 buttonStatus.forEach((item) => {
   let status;
@@ -11,5 +14,32 @@ buttonStatus.forEach((item) => {
     path = ` ${formElement.getAttribute('data-path')}/change-status/${status}/${id}?_method=PATCH`;
     formElement.action = path;
     formElement.submit();
+  })
+})
+
+checkBoxAll.addEventListener('click', () => {
+  if (checkBoxAll.checked) {
+    checkBoxRecord.forEach((item) => {
+      item.checked = true;
+    })
+  } else {
+    checkBoxRecord.forEach((item) => {
+      item.checked = false;
+    })
+  }
+})
+
+checkBoxRecord.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (item.checked === true) {
+      total++;
+    } else {
+      checkBoxAll.checked = false;
+    }
+    if (total === checkBoxRecord.length) {
+      checkBoxAll.checked = true;
+    } else {
+      checkBoxAll.checked = false;
+    }
   })
 })
