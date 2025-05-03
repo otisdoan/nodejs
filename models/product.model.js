c250557da69f6c67396aedbbbac23685e2131651
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-03 14:56:22 +0700

    slug

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema({
   title: String,
@@ -18,6 +20,7 @@ const productSchema = new mongoose.Schema({
   position: Number,
   thumbnail: String,
   deletedAt: Date,
+  slug: { type: String, slug: "title" }
 }, {
   timestamps: true
 })
