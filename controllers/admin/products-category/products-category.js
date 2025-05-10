8a2412ff05b75661564df8d9a62f84a1c1489668
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-10 20:28:14 +0700

    update

--- controllers/admin/products-category/products-category.js
+++ controllers/admin/products-category/products-category.js
@@ -1,10 +1,13 @@
 const ProductCategory = require('../../../models/products-category.model')
 
-module.exports.index = (req, res) => {
-  res.render('admin/pages/products-category/index');
+module.exports.index = async (req, res) => {
+  const category = await ProductCategory.find({ deleted: false });
+  res.render('admin/pages/products-category/index', {
+    category: category
+  });
 }
 
-module.exports.createUI = (req, res) => {
+module.exports.createUI = async (req, res) => {
   res.render('admin/pages/products-category/create');
 }
 
