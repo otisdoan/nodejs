0f5a36be929b57c9486d90d20f1dd81860d55af0
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-11 16:36:23 +0700

    update

--- controllers/admin/products-category/products-category.js
+++ controllers/admin/products-category/products-category.js
@@ -8,7 +8,24 @@ module.exports.index = async (req, res) => {
 }
 
 module.exports.createUI = async (req, res) => {
-  res.render('admin/pages/products-category/create');
+  const category = await ProductCategory.find({ deleted: false });
+  function createTree(arr, parentId = '') {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id === parentId) {
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          item.children = children;
+        }
+        tree.push(item)
+      }
+    })
+    return tree;
+  }
+  const newCategory = createTree(category);
+  res.render('admin/pages/products-category/create', {
+    category: newCategory
+  });
 }
 
 module.exports.createPost = async (req, res) => {
